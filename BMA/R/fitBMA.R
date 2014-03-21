ff0147acd24081164183f9263e26f9cd26f74e9d
thomasscarroll89 <thomasscarroll89>
2014-03-20 21:48:08 -0500

    Updated fitBMA
    Updated main function so variables would be standardized and the output
    would reflect this (i.e. getting rid of the column in the output that
    contained the results from a model that included only an intercept)

--- BMA/R/fitBMA.R
+++ BMA/R/fitBMA.R
@@ -15,36 +15,52 @@ setGeneric("fitBMA", function(object="BMAdata"){
 #' @export
 setMethod("fitBMA", "BMAdata", function(object="BMAdata"){
   k <- ncol(object@covariates) #Let k be the number of predictor variables (not including intercept)
-  colnames(object@covariates) <- paste(rep("X", length=k), 1:k, sep="")
-  model.combinations <- vector(mode="list") #Create object "model.combinations", which is a list containing all of the different combinations of variables 
-  number.of.models <- c(1) #do 1, for the model with just an intercept
+  
+  #FIRST we need to standardize the variables.
+  y.stand <- (object@depvar - mean(object@depvar))/sd(object@depvar)
+  x.stand <- matrix(NA, nrow=nrow(object@covariates), ncol=k)
   for(i in 1:k){
-    model.combinations[[i]] <- combn(c(1:k), m=i)
-    number.of.models <- append(number.of.models, values=ncol(model.combinations[[i]]), after=length(number.of.models))
+    x.stand[,i] <- (object@covariates[,i] - mean(object@covariates[,i]))/sd(object@covariates[,i])
   }
-  #CREATE basic structure of the output matrix
-  output2 <- matrix(NA, nrow=k+2, ncol=sum(number.of.models))
-  rownames(output2) <- c("Intercept", colnames(object@covariates), "R^2")
-  colnames(output2) <- paste(rep("Model", length=sum(number.of.models)), 1:sum(number.of.models), sep=" ")
-  #BEGIN running linear models; start with model using only an intercept
-  model.base <- lm(object@depvar ~ 1)
-  model.base.coef <- model.base$coef
-  output2[1,1] <- model.base.coef
-  r.squared <- summary(model.base)$r.squared  
-  count <- 1
-  for(i in 1:length(model.combinations)){
-    for(j in 1:ncol(model.combinations[[i]])){
-      count <- count + 1
-      variable.numbers <- model.combinations[[i]][,j]
-      model <- lm(object@depvar ~ object@covariates[,c(variable.numbers)])
-      coefficients <- model$coef
-      output2[c(1, variable.numbers + 1),count] <- coefficients
-      r.squared <- append(r.squared, values=summary(model)$r.squared, after=length(r.squared))
+  colnames(x.stand) <- paste(rep("X", length=k), 1:k, sep="") #add variable names to keep track of the variables more easily
+  
+  #SECOND we run every possible combination of linear models (none including an intercept, since we standardized the variables)
+    #2.1, we will calculate just how many linear models we need to run. The following for loop does this. 
+    model.combinations <- vector(mode="list") #Create object "model.combinations", which is a list containing all of the different combinations of variables 
+    number.of.models <- c(0) #Use 0 since we don't have any models where there's just an intercept
+    for(i in 1:k){
+      model.combinations[[i]] <- combn(c(1:k), m=i)
+      number.of.models <- append(number.of.models, values=ncol(model.combinations[[i]]), after=length(number.of.models))
     }
-  }
-  output2[k+2,] <- r.squared  
-  output <- as.data.frame(output2)
-  output <- round(output, 4)
-  output[is.na(output)] <- ""
-  return(output)
-})
+    q <- sum(number.of.models) #q is just the total number of models we will need to run
+    #2.2, we'll make a quick test to ensure that the number of models we are going to run is equal to how many models we should, theoretically, have
+    q.test <- (2^k)-1
+    if(q.test!=q){
+      warning("Number of models being run does not equal how many models there should, theoretically, be")
+    }
+
+  #THIRD, we run the actual models.
+    #3.1, we create the basic structure of the output matrix
+    output2 <- matrix(NA, nrow=k+1, ncol=q)
+    rownames(output2) <- c(colnames(x.stand), "R^2")
+    colnames(output2) <- paste(rep("Model", length=q), 1:q, sep=" ")
+    #3.2, we run linear models. The for loops run 2^k - 1 models, one at a time. 
+    count <- 0  #The "count" variable keeps track of which model number we are currently on; it helps when we're plugging in coefficient
+                # values and R squared values by basically telling us which column of the output matrix
+                #to plug these values into
+    for(i in 1:length(model.combinations)){
+      for(j in 1:ncol(model.combinations[[i]])){
+        count <- count + 1
+        variable.numbers <- model.combinations[[i]][,j]
+        model <- lm(y.stand ~ x.stand[,c(variable.numbers)] - 1)
+        coefficients <- model$coef
+        output2[c(variable.numbers),count] <- coefficients
+        output2[c(k+1),count] <- summary(model)$r.squared
+      }
+    }
+    #3.3 Next we clean up the output a little bit by doing some rounding and replacing NAs with empty character strings. 
+    output <- as.data.frame(output2)
+    output <- round(output, 4)
+    output[is.na(output)] <- ""
+    return(output)
+  })
