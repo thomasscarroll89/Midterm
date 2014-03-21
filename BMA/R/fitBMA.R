e984ae9d06acb3df4224f6321cd56a9fd2fe8d6f
thomasscarroll89 <thomasscarroll89>
2014-03-20 22:15:04 -0500

    Posterior Model Odds
    Included code to calculate posterior model odds.

--- BMA/R/fitBMA.R
+++ BMA/R/fitBMA.R
@@ -13,9 +13,9 @@ setGeneric("fitBMA", function(object="BMAdata"){
 })
 
 #' @export
-setMethod("fitBMA", "BMAdata", function(object="BMAdata"){
+setMethod("fitBMA", "BMAdata", function(object="BMAdata", g=3){
   k <- ncol(object@covariates) #Let k be the number of predictor variables (not including intercept)
-  
+
   #FIRST we need to standardize the variables.
   y.stand <- (object@depvar - mean(object@depvar))/sd(object@depvar)
   x.stand <- matrix(NA, nrow=nrow(object@covariates), ncol=k)
@@ -59,8 +59,20 @@ setMethod("fitBMA", "BMAdata", function(object="BMAdata"){
       }
     }
     #3.3 Next we clean up the output a little bit by doing some rounding and replacing NAs with empty character strings. 
-    output <- as.data.frame(output2)
-    output <- round(output, 4)
-    output[is.na(output)] <- ""
-    return(output)
+    output.1 <- as.data.frame(output2)
+    output.1 <- round(output.1, 4)
+    output.1[is.na(output.1)] <- ""
+    # So output.1 is a matrix of coefficient estimates and R^2 values for each model
+
+  #FOURTH, we need to calculate the posterior model odds for each model. Here we make use of the formula
+    #provided in the slides.
+  B.Mk.MO <- numeric(length=q) #this will be the vector that we store the values of B[M_{k}:M_{0}] into for each model. See slide 25
+  n <- nrow(x.stand) #let n be the number of observations
+  for(i in 1:q){
+    p.sub.k <- sum(which(output.1[,i]!="")) - 1 #this represents number of independent variables in a given model. We subtract one because the R^2 value is recorded in output.1 and we don't want to count that as an independent variable
+    r.squared.k <- output.1[k+1,i] #let this represent the R^2 value in a given model
+    B.Mk.M0[i] <- ((1 + g)^((n - p.sub.k - 1)/2))*((1 + (g*(1 - r.squared.k)))^(-((n - 1)/2)))
+  }
+  posterior.model.odds <- B.Mk.M0/sum(B.Mk.M0) #posterior.model.odds is p(M_{k}|Y) on slide 10
+  
   })
